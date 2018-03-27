0703018292ba49e5cb48167fdc96ca382efda0d9
Muhammed Jobe <thejobemuhammed@gmail.com>
2018-03-27 01:12:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ var helmet = require('helmet')
 
 //app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
 
-/*var limiter = new RateLimit({
+var limiter = new RateLimit({
   windowMs: 15*60*1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   delayMs: 0 // disable delaying - full speed until the max limit is reached
-});*/
+});
 
 //  apply to all requests
-//app.use(limiter);
+app.use(limiter);
 
 app.use(helmet({
   frameguard: false
